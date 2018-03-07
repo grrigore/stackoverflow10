201f0b46a6e73ded359e69c0f7c447bda120aa9d
Cristian-Andrei Grigore <cristi9627@yahoo.com>
2018-03-07 22:51:44 +0200

    implemented the 2 activities

--- app/src/main/java/com/example/grrigore/stackoverflow10/UserActivity.java
+++ app/src/main/java/com/example/grrigore/stackoverflow10/UserActivity.java
@@ -2,13 +2,16 @@ package com.example.grrigore.stackoverflow10;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class UserActivity extends AppCompatActivity {
 
     private TextView userNameTextView;
-    private ImageView userProfilePictureTextView;
+    private ImageView userProfilePictureImageView;
     private TextView userLocationTextView;
     private TextView userBronzeBadgeTextView;
     private TextView userSilverBadgeTextView;
@@ -20,6 +23,22 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        userNameTextView = findViewById(R.id.user_name);
+        userProfilePictureImageView = findViewById(R.id.user_profile_picture);
+        userLocationTextView = findViewById(R.id.user_location);
+        userBronzeBadgeTextView = findViewById(R.id.bronze_badges);
+        userSilverBadgeTextView = findViewById(R.id.silver_badges);
+        userGoldBadgeTextView = findViewById(R.id.gold_badges);
+
         User user = getIntent().getParcelableExtra("userData");
+
+        Log.d("USER", user.toString());
+
+        userNameTextView.setText(user.getUserName());
+        Picasso.with(this).load(user.getUserProfilePicture()).resize(240,240).into(userProfilePictureImageView);
+        userLocationTextView.setText(user.getUserLocation());
+        userBronzeBadgeTextView.setText(String.valueOf(user.getUserBronzeBadge()));
+        userSilverBadgeTextView.setText(String.valueOf(user.getUserSilverBadge()));
+        userGoldBadgeTextView.setText(String.valueOf(user.getUserGoldBadge()));
     }
 }
